9bb84153458a54ad95968978bb17a85a9035c5ab
najmHassan <nhh4@njit.edu>
2020-09-19 00:24:24 -0400

    HW3 PHP file

--- hw3.php
+++ hw3.php
@@ -1,11 +1,15 @@
 <?php
 $arr = [1,2,3,4,5,6,7,8,9];
-echo "$num";
+
+echo "numbers in array are:<br>";
 foreach($arr as $num){
-    echo "$num";
+    echo "$num <br>";
+    }
+    
+echo "even numbers in array are:<br>"; 
+foreach($arr as $num){
  if($num%2==0){
-  echo "$num <br>\n";
+  echo "$num <br>";
   }
 }
-
 ?>
