7f11c48539f52c52ee6c6c4afb4fcccf65185d09
najmhassan <nhh4@njit.edu>
2020-11-28 21:29:43 -0500

    added features to editProduct,ViewProduct, to add items to cart

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -14,46 +14,41 @@ require_once(__DIR__ . "/../lib/helpers.php");
 
 
 <bla class="navbar navbar-expand-lg navbar-light bg-light">
-  <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-  <a class="navbar-brand" href="home.php">Y2K</a>
+  <a class="navbar-brand" href="home.php"><img src="photos/Y2K.png" width="50" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+  </a>
 
   <?php if (is_logged_in()): ?>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+  <form name="form" action="searchResults.php" method="get" style ="display:inline-flex;">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-  <div class="input-group"style="width: 500;">
+  <div class="input-group"style="width: 500; position:relative; left:250;">
     <span class="input-group-prepend">
       <div class="input-group-text bg-transparent border-right-0">
       <i class="fa fa-search" ></i>
       </div>
     </span>
-    <form name="form" action="" method="get">
     <input class="form-control py-2 border-left-0 border" type="search" name="search" placeholder="search" id="search" />
-  </form>
-<?php
-$search = "";
-if(isset($_GET["search"])){
-  $search = $_GET["search"];
-  echo "$search";
-}
+    <?php
+    $search = "";
+    if(isset($_GET["search"])){
+      $search = $_GET["search"];
+    }
  ?>
-
     <span class="input-group-append">
-      <button class="btn btn-light border-left-0 border" type="button">
-        <a href="searchResults.php?search=<?php echo "$search";?>">
-
-        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-          <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-        </a>
-
-      </svg>
-    </button>
+      <button class="btn btn-light border-left-0 border" type="submit"> <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+  <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+</svg>
+</button>
     </span>
+    </form>
+
 </div >
-      <a href="MyCart.php" style="margin-left: 50%;"">
+      <a href="MyCart.php" style="position: relative;left: 55%;">
       <button style="color: black;"  type="button" class = "btn pull-right">
         <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-cart4" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
@@ -67,7 +62,9 @@ if(isset($_GET["search"])){
 
 
 
-<nav class="navbar navbar-expand-lg  text-white navbar-dark bg-secondary ">
+<nav style= "background-color:#3e6a49;" class="navbar navbar-expand-lg text-white navbar-dark bg-dark " style = ".bg-dark {
+background-color: #4c885b!important;
+}"  >
 
 
 
@@ -91,22 +88,19 @@ if(isset($_GET["search"])){
       <?php endif; ?>
 
 
-
+      <li class="nav-item">
+        <a class="nav-link" href="myProducts.php">PRODUCTS</a>
+      </li>
 
       <?php if (is_logged_in()): ?>
           <li class="nav-item">
             <a class="nav-link" href="profile.php">profile</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="myProducts.php">PRODUCTS</a>
-          </li>
 
           <li class="nav-item">
             <a class="nav-link" href="logout.php">logout</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="test_list_product.php"> Search Products</a>
-          </li>
+
       <?php endif; ?>
       <?php if (has_role("Admin")): ?>
       <li class="nav-item dropdown">
--- project/searchResults.php
+++ project/searchResults.php
@@ -1,6 +1,6 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
-if (!has_role("Admin")) {
+if (!is_logged_in()) {
     //this will redirect to login and kill the rest of this script (prevent it from executing)
     flash("You don't have permission to access this page");
     die(header("Location: login.php"));
@@ -8,21 +8,21 @@ if (!has_role("Admin")) {
 ?>
 <?php
 
-$query = "";
+
 $results = [];
-if (isset($_POST["query"])) {
-    $query = $_POST["query"];
+$query = "";
+if(isset($_GET["search"])){
+  $query = $_GET["search"];
 }
-if (isset($_POST["search"]) && !empty($query)) {
+
+if (isset($_GET["search"]) && !empty($query)) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT Cart.id, Cart.price, Users.username from Cart JOIN Users on Users.id = Cart.user_id WHERE Users.username like :q LIMIT 10");
+    $stmt = $db->prepare("SELECT * FROM Products WHERE name like :q AND visibility = 1 LIMIT 10");
 
     $r = $stmt->execute([":q" => "%$query%"]);
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         //var_export($results);
-
-
     }
     else {
         flash("There was a problem fetching the results " . var_export($stmt->errorInfo(), true));
@@ -32,32 +32,21 @@ if (isset($_POST["search"]) && !empty($query)) {
 
 
 ?>
-<h3>List of Products in Cart </h3>
-<form method="POST">
-    <input name="query" placeholder="Search by username" value="<?php safer_echo($query); ?>"/>
-    <input type="submit" value="Search" name="search"/>
-</form>
-<div class="results">
-    <?php if (count($results) > 0): ?>
-        <div class="list-group">
-            <?php foreach ($results as $r): ?>
-                <div class="list-group-item">
-                  <div>
-                     <div>Cart ID: <?php safer_echo($r["id"]); ?></div>
-                 </div>
-                 <div>
-                     <div>Price Total: <?php safer_echo($r["price"]); ?></div>
-                 </div>
-                 <div>
-                     <div>Creator: <?php safer_echo($r["username"]); ?></div>
-                 </div>
-                 <div>
-                   <a type="button" href="test_edit_cart.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
-                   <a type="button" href="test_view_cart.php?id=<?php safer_echo($r['id']); ?>">View</a>
-                </div>
-            <?php endforeach; ?>
+<h1>SEARCH RESULTS</h1>
+<div class="row" style= "margin-left: 4em;">
+<?php if (count($results) > 0): ?>
+    <?php foreach ($results as $r): ?>
+      <div   class="card" style="width: 20rem; margin: 1em;">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+          <a href = "ViewProduct.php?id=<?php safer_echo($r['id']); ?>" <h5 class="card-title"><?php safer_echo($r["name"]); ?></h5></a>
+          <h6 class="card-title"><?php safer_echo($r["price"]); ?></h6>
+          <p class="card-text"><?php safer_echo($r["description"]); ?></p>
+          <?php if (has_role("Admin")): ?>
+            <a href="test_view_product.php?id=<?php safer_echo($r['id']); ?>" class="btn btn-primary">Edit</a>
+          <?php endif; ?>
+          </div>
         </div>
-    <?php else: ?>
-        <p>No results</p>
-    <?php endif; ?>
+<?php endforeach; ?>
+<?php endif; ?>
 </div>
